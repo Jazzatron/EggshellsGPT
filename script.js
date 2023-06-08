2c4b80aa1d656f8282b265edad5f0fb9fbbf1b50
SaraHaxhia <sara_haxhia@hotmail.com>
2023-06-08 17:14:06 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+//TO DO: make this work for the enter button too
+const chatInput = document.getElementById("chat-input");
+const chatHistory = document.getElementById("chat-history");
+let previousPrompts =
+  "You are a tutor that always responds in the Socratic style. You *never* give the student the answer, but always try to ask just the right question to help them learn to think for themselves. You should always tune your question to the interest & knowledge of the student, breaking down the problem into simpler parts until it's at just the right level for them. You should be empathetic and compassionate in every message. Here is the previous conversation history, up until the latest question asked. ";
+
+function sendMessage() {
+  if (chatInput.value) {
+    var userMessage = document.createElement("p");
+    userMessage.textContent = chatInput.value;
+    chatHistory.appendChild(userMessage);
+    previousPrompts += ` User: ${chatInput.value} AI:`;
+
+    fetch("http://localhost:3000/api/gpt-3", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: previousPrompts,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        var chatbotMessage = document.createElement("p");
+        chatbotMessage.textContent =
+          data?.choices?.[0]?.message?.content ??
+          "Whoops, I'm busy right now. Can you try again in a minute?";
+        chatbotMessage.style.color = "#EB9600";
+        chatHistory.appendChild(chatbotMessage);
+        previousPrompts += chatbotMessage.textContent;
+      })
+      .catch((error) => {
+        var chatbotMessage = document.createElement("p");
+        chatbotMessage.textContent =
+          "Whoops, I'm busy right now. Can you try again in a minute?";
+        chatbotMessage.style.color = "#EB9600";
+        chatHistory.appendChild(chatbotMessage);
+        console.error(error);
+      });
+
+    chatInput.value = "";
+  }
+}
+
+document.querySelector("#send-button").addEventListener("click", sendMessage);
+chatInput.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") sendMessage();
+});
+/* var chatbotMessage = document.createElement('p');
+    chatbotMessage.textContent = 'Hey, you';
+    chatbotMessage.style.color = '#EB9600';
+    chatHistory.appendChild(chatbotMessage); */
---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { config } = require("dotenv");
+const { Configuration, OpenAIApi } = require("openai");
+
+// Load environment variables from .env file
+config();
+
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+  })
+);
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(require("cors")());
+
+app.post("/api/gpt-3", async (req, res) => {
+  const userMessage = req.body.message;
+  console.log('Received message:', userMessage);
+
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: userMessage }],
+      max_tokens: 16, 
+      temperature: 0.5
+    });
+    console.log("Response from OpenAI API:", response);
+    res.json(response.data);
+  } catch (error) {
+    //console.error("Error from OpenAI API:", error);
+    res.status(500).json({ error: error.toString() });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+
+//To add in future: 
+//More specific error responses
+//security (authentication or authorization layers, rate limiting)
