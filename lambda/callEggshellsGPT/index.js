c50d83ae2785e3b36d6d86976eddb874feb86291
Jazzatron <jaskaran.singh.kahlon@gmail.com>
2023-09-30 21:52:38 +0100

    feat: set up persisting to dynamo

--- lambda/callEggshellsGPT/index.js
+++ lambda/callEggshellsGPT/index.js
@@ -1,4 +1,10 @@
 const { OpenAI } = require("openai")
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {DynamoDBDocumentClient, PutCommand} = require("@aws-sdk/lib-dynamodb")
+
+const dynamoClient = new DynamoDBClient({});
+const dynamo = DynamoDBDocumentClient.from(dynamoClient)
+
 console.log('Loading functions');
 
 const openai = new OpenAI(
@@ -10,26 +16,37 @@ const openai = new OpenAI(
 exports.handler = async (event, context) => {
   const masterPrompt = process.env.WORRY_TIME_PROMPT
   const masterMessage = {role: "system", content: masterPrompt}
-  console.log("Event: ", event)
   
   const cleanedMessages = event.messages.filter((message)=>message.role!=="system")
-  const userMessages = event.messages;
-  console.log('Received message:', userMessages);
+  const converstaionHistory = event.messages;
+  
+  if(!event.id_conversation || !event.unix_timestamp) {
+    console.error("No missing id_conversation or unix_timestamp")
+    throw Error("Bad request - missing properties")
+  }
 
-  let response
+  let eggshellResponse
   try {
-    response = await openai.chat.completions.create({
+    const response = await openai.chat.completions.create({
       model: "gpt-4",
-      messages: [masterMessage, ...userMessages],
+      messages: [masterMessage, ...converstaionHistory],
       max_tokens: 1024,
       temperature: 0,
     });
-    console.log("Response from OpenAI API:", response.choices[0].message);
+    
+    eggshellResponse = response.choices[0].message
   } catch (error) {
     console.error("Error from OpenAI API:", error);
-    throw error
+    throw error   
   }
-    
-  const result = response.choices[0].message
-  return result;
+  
+  try {
+    const conversationItem = {id_conversation:event.id_conversation, unix_timestamp: event.unix_timestamp, master_prompt: "worry_time", user_messages: [...converstaionHistory, eggshellResponse]}  
+    const conversationPutCommand = new PutCommand({TableName: "conversations", Item: conversationItem})   
+    await dynamo.send(conversationPutCommand)
+  } catch (error) {
+    console.error("Error from DynamoDB: ", error)
+  }
+
+  return eggshellResponse;
 };
