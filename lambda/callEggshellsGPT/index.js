41d5f7218ac5b141e12745cc9bbe65f6fb767542
Jazzatron <jaskaran.singh.kahlon@gmail.com>
2023-10-05 18:35:33 +0100

    feat: Store master prompts in dynamo DB

--- lambda/callEggshellsGPT/index.js
+++ lambda/callEggshellsGPT/index.js
@@ -17,18 +17,20 @@ exports.handler = async (event, context) => {
   const masterPrompt = process.env.WORRY_TIME_PROMPT
   const masterMessage = {role: "system", content: masterPrompt}
   
-  const conversationHistory = event.messages.filter((message)=>message.role!=="system")
-  
+  const cleanedMessages = event.messages.filter((message)=>message.role!=="system")
+  const conversationHistory = event.messages;
+
   if(!event.id_conversation || !event.unix_timestamp) {
     console.error("No missing id_conversation or unix_timestamp")
     throw Error("Bad request - missing properties")
   }
 
+  let conversationHistoryWithMasterPrompt = [masterMessage, ...conversationHistory]
   let eggshellResponse
   try {
     const response = await openai.chat.completions.create({
       model: "gpt-4",
-      messages: [masterMessage, ...conversationHistory],
+      messages: conversationHistoryWithMasterPrompt,
       max_tokens: 1024,
       temperature: 0,
     });
@@ -40,7 +42,7 @@ exports.handler = async (event, context) => {
   }
   
   try {
-    const conversationItem = {id_conversation:event.id_conversation, unix_timestamp: event.unix_timestamp, master_prompt: "worry_time", user_messages: [...conversationHistory, eggshellResponse]}  
+    const conversationItem = {id_conversation:event.id_conversation, unix_timestamp: event.unix_timestamp, user_messages: [...conversationHistoryWithMasterPrompt, eggshellResponse]}  
     const conversationPutCommand = new PutCommand({TableName: "conversations", Item: conversationItem})   
     await dynamo.send(conversationPutCommand)
   } catch (error) {
